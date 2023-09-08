9ef34a6c591e94a57ae50fed32d6bd2871ac3323
Ericokurniawan <143327500+Ericokurniawan@users.noreply.github.com>
2023-09-08 14:33:30 +0700

    Add files via upload

---
+++ phpmvc/app/view/Home/index.php
@@ -0,0 +1,18 @@
+<style>
+   body {
+    background-image: url(http://localhost/phpmvc/img/CODING.jpg);
+   }
+    .jumbotron{
+        background-color: wheat;
+    }
+
+
+</style>
+<div class="container">
+    <div class="jumbotron mt-4">
+        <h1 class="display-4"> Heloo, Welcome to Smkn 2 Trenggalek</h1>
+        <hr class="my-4">
+        <p>Sekolah Menengah Kejuruan Negeri 2 Trenggalek</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
+    </div>
+</div>
